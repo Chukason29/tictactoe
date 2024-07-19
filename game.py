42381c971a1bd87fc4dd017824a359e4a022f2a4
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-19 18:40:02 +0100

    Created Players

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
-print("Hello World")
+class Game:
+    #TODO create class variable
+    winner = False #TODO  bool to check for a winner
+
+    #TODO create instance variables of player. playername, status, sign
+    def __init__(self, name: str, status: bool, sign: str):
+        self.name = name
+        self.status = status
+        self.sign = sign
+
+    
+    #TODO create a method to create player instance based on the input of each player
+    
+
+    #TODO create a method for each player to play its turn and return to the next player if there is no winner
+
+player1 = Game(input("Player1 Name: "), False, input("Player1 Sign: "))
+player2 = Game(input("Player2 Name: "), False, input("Player2 Sign: "))
+
+print(player1)
+print(player2)
