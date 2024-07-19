0209200dc7dea8a225bffd2ac5adb23454ac6f57
Chukason29 <Chukwuebukaalaegbu@gmail.com>
2024-07-19 19:04:56 +0100

    added game boxes

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 class Game:
     #TODO create class variable
-    winner = False #TODO  bool to check for a winner
+    winner = False  #bool to check for a winner
+    game_boxes = [
+        ["O", "O", "O"],
+        ["O", "O", "O"],
+        ["O", "O", "O"],
+    ]
+
 
     #TODO create instance variables of player. playername, status, sign
     def __init__(self, name: str, status: bool, sign: str):
@@ -14,8 +20,10 @@ class Game:
 
     #TODO create a method for each player to play its turn and return to the next player if there is no winner
 
+
+    #TODO use the __repr__() to show the object format
+    def __repr__(self):
+        return f"Player({self.name}, {self.status}, {self.sign})"
+
 player1 = Game(input("Player1 Name: "), False, input("Player1 Sign: "))
 player2 = Game(input("Player2 Name: "), False, input("Player2 Sign: "))
-
-print(player1)
-print(player2)
